4c17f5a573026859f963cc8d714e0654c45d73e1
Matioma <Artiom.Matievschi@gmail.com>
2020-10-12 09:29:18 +0200

    Update project structure
    Testing FUnctionality

--- Assigment/Assigment/main.cpp
+++ Assigment/Assigment/main.cpp
@@ -7,11 +7,18 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1200, 800), "SFML works!");
     window.setFramerateLimit(60);
 
+    sf::Texture texture;
+    if (!texture.loadFromFile("../Resources/CharacterPlaceHolder1.png"))
+    {
+        printf_s("unable to load find the texture");
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setPosition(sf::Vector2f(50,0));
 
     while (window.isOpen())
     {
-      
-
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -22,7 +29,8 @@ int main()
             }
         }
         window.clear();
-        window.display();
+        window.draw(sprite);
+        window.display() ;
     }
     return 0;
 }
